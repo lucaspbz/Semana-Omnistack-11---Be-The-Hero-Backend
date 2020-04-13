60a16596ba3ddba3ac744e472606f42ae87d3fb0
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-04-13 00:03:59 -0300

    Backend da App

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+// npm start para iniciar o servidor com o nodemon
+
+app.use(express.json());
+
+
+gapp.use(cors());
+app.use(routes);
+
+app.listen(3333);
