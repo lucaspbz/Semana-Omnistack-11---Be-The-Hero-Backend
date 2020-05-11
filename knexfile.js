1750bc7dda30b8d5cafe02ad2738fe73975fb569
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-05-11 15:33:10 -0300

    Commit test

--- knexfile.js
+++ knexfile.js
@@ -3,48 +3,46 @@
 module.exports = {
 
 
-//npx knex migrate:make nome_da_migration para criar migrations 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
+  //npx knex migrate:make nome_da_migration para criar migrations 
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './src/database/db.sqlite'
+      },
+      migrations:{
+        directory: './src/database/migrations'
+      },
+      useNullAsDefault: true,
     },
-    migrations:{
-      directory: './src/database/migrations'
+  
+    staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
     },
-    useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+  
+    production: {
+      client: 'postgresql',
+      connection: process.env.DATABASE_URL,
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        directory: __dirname + '/src/database/migrations' ,
+        tableName: 'knex_migrations'
+      }
     }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+  
+  };
+  
