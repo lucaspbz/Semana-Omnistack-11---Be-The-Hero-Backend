99aab327676c761338b11d530e7995b44f8dbf38
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-07-08 10:19:00 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 // npm start para iniciar o servidor com o nodemon
 
 app.use(express.json());
-
 
 app.use(cors());
 app.use(routes);
