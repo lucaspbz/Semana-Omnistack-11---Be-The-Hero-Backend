a7db2c606c8444b741b3e47bd2bf7f1572813774
Lucas Pinheiro Bezerra <lucas.pbezerra@hotmail.com>
2020-07-08 10:08:37 -0300

    Another cors fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'https://lucaspb-bethehero.netlify.app/',
+    origin: 'https://lucaspb-bethehero.netlify.app',
   }));
 app.use(routes);
 
